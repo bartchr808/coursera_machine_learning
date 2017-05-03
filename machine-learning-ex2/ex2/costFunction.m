635c4cb83447624903e03e4bc80fb7008f9674d8
bartchr808 <bart.chr@gmail.com>
2017-05-03 00:47:27 -0400

    Completed some of the second assignment.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,11 +20,17 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-J = (1/m)*sum(-y'*log(sigmoid(X*theta)) - (1-y')*(log(1-sigmoid(X*theta))));
+h = sigmoid(X*theta);
 
-grad = (1/m)*sum((sigmoid(X*theta) - y)'*X)
+%J = (1/m)*sum(-y'*log(h) - (1-y')*(log(1-sigmoid(h))));
 
+%grad = (1/m)*sum((sigmoid(X*theta) - y)'*X)
+%grad = 1/m * (X' * (h - y));
+J = 1 / m * sum(-y .* log(h) - (1 - y) .* log(1 - h));
 
+for i = 1:size(theta, 1)
+    grad(i) = 1 / m * sum((h - y) .* X(:, i));
+end
 
 
 

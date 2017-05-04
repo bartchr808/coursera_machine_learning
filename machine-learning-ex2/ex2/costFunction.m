b788fd133fbea477205dd1d06fbb45346a8a4869
bartchr808 <bart.chr@gmail.com>
2017-05-04 00:13:55 -0400

    Finished assignment 2.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -28,8 +28,8 @@ h = sigmoid(X*theta);
 %grad = 1/m * (X' * (h - y));
 J = 1 / m * sum(-y .* log(h) - (1 - y) .* log(1 - h));
 
-for i = 1:size(theta, 1)
-    grad(i) = 1 / m * sum((h - y) .* X(:, i));
+for j = 1:size(theta, 1)
+    grad(j) = 1 / m * sum((h - y) .* X(:, j));
 end
 
 
--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -14,14 +14,9 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters.
 %               You should set p to a vector of 0's and 1's
 %
-n_theta = sigmoid(X * theta);
+n_theta = sigmoid(X*theta);
 
 p = (n_theta >= 0.5);
-
-
-
-
-
 % =========================================================================
 
 
